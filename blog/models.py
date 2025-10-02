6c7400e626a5da3f77315f08f455182996455af1
daniel4191 <daniell8282@naver.com>
2025-10-02 15:45:36 +0900

    blog앱에 Post, Comment 기본 모델 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField("제목", max_length=50)
+    content = models.TextField("내용")
+    thumbnail = models.ImageField("썸네일", upload_to="post", blank="True")
+    
+    def __str__(self):
+        return f"{self.title}"
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.TextField("댓글")
+    
+    def __str__(self):
+        return f"post: {self.post}, comment: {self.comment}"
