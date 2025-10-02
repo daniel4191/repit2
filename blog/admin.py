2f13d7b4f7845e1d3dc4dc89d996385d6fb5d98e
daniel4191 <daniell8282@naver.com>
2025-10-02 16:42:33 +0900

    구성은 모두 완료, css작업만 남음

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Post, Comment
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # list_display = ["title", "thumbnail"]
+    pass
+    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
